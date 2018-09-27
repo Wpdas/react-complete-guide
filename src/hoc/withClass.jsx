6ea128345f53d168be891ec4ddf823443a073065
Wenderson Pires <wpdas@yahoo.com.br>
2018-09-27 12:30:02 -0300

    HOC like a Component

--- src/hoc/withClass.jsx
+++ src/hoc/withClass.jsx
@@ -1,14 +1,29 @@
-// Props Proxy. Should be used like a configuration HOC. Name starts with lowercase.
 // CONFIGURATION
 // Due to this, you should NEVER MANIPULATES the WrappedComponent
 // This is a funnction that returns a function containing the modified Wrapped Component
 import React from 'react';
+
+// As a Simple Function
+
+// const withClass = (WrappedComponent, className) => {
+//   return props => (
+//     <div className={className}>
+//       <WrappedComponent {...props} />
+//     </div>
+//   );
+// };
+
+// As a component now: (maybe you can manipulate it)
 const withClass = (WrappedComponent, className) => {
-  return props => (
-    <div className={className}>
-      <WrappedComponent />
-    </div>
-  );
+  return class extends React.Component {
+    render() {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props} />
+        </div>
+      );
+    }
+  };
 };
 
 export default withClass;
