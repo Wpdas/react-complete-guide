dd5d471b5de268a30e090c3f079f7058421bd5cd
Wenderson Pires <wpdas@yahoo.com.br>
2018-10-29 02:30:56 -0300

    Adding authentication to Burger Project - Complete

--- assignments/burguer-builder-app/src/App.jsx
+++ assignments/burguer-builder-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -25,21 +25,39 @@ class App extends Component {
   //   });
   // }
   render() {
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} exact />
+        <Route path="/" component={BurgerBuilder} exact />
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} exact />
+          <Route path="/logout" component={Logout} exact />
+          <Route path="/" component={BurgerBuilder} exact />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
     return (
       <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} exact />
-            <Route path="/auth" component={Auth} exact />
-            <Route path="/logout" component={Logout} exact />
-            <Route path="/" component={BurgerBuilder} exact />
-          </Switch>
-        </Layout>
+        <Layout>{routes}</Layout>
       </div>
     );
   }
 }
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
+  };
+};
 
 const mapDispatchToProps = dispatch => {
   return {
@@ -49,7 +67,7 @@ const mapDispatchToProps = dispatch => {
 
 export default withRouter(
   connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
   )(App)
 );
