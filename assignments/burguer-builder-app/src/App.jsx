6a3876ba27c039557da24811b64e712555a04351
Wenderson Pires <wpdas@yahoo.com.br>
2018-10-30 00:09:24 -0300

    Adding Lazy Loading.

--- assignments/burguer-builder-app/src/App.jsx
+++ assignments/burguer-builder-app/src/App.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import asyncComponent from './hoc/asyncComponent/asyncComponent';
 
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Connect from './Connect';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from './containers/Auth/Auth';
+
 import Logout from './containers/Auth/Logout/Logout';
 import * as actions from './store/actions/index';
+
+// Lazy Loading
+const asyncCheckout = asyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const asyncOrders = asyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
+
+const asyncAuth = asyncComponent(() => {
+  return import('./containers/Auth/Auth');
+});
 
 // export const appConnect = new Connect();
 
@@ -27,7 +39,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth} exact />
+        <Route path="/auth" component={asyncAuth} exact />
         <Route path="/" component={BurgerBuilder} exact />
         <Redirect to="/" />
       </Switch>
@@ -36,10 +48,10 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} exact />
+          <Route path="/checkout" component={asyncCheckout} />
+          <Route path="/orders" component={asyncOrders} exact />
           <Route path="/logout" component={Logout} exact />
-          <Route path="/auth" component={Auth} exact />
+          <Route path="/auth" component={asyncAuth} exact />
           <Route path="/" component={BurgerBuilder} exact />
           <Redirect to="/" />
         </Switch>
